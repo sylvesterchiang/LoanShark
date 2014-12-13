fc0e7b36b15ff14b6ec6da87ff8cf79273c72a96
Sylvester Chiang <sylvester_ch7593@hotmail.com>
2014-12-13 07:20:04 -0500

    Show Transaction
    Sum Display not working correctly

--- app/src/main/java/com/example/sylve_000/loanshark/TransactionActivity.java
+++ app/src/main/java/com/example/sylve_000/loanshark/TransactionActivity.java
@@ -5,14 +5,20 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.parse.ParseACL;
 import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseQueryAdapter;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
+
+import java.text.SimpleDateFormat;
 
 /**
  * Created by sylve_000 on 2014-12-11.
@@ -23,6 +29,8 @@ public class TransactionActivity extends Activity{
     private String currentUserId;
     private ListView transactionList;
     private EditText amountField;
+    private ParseQueryAdapter <Transaction> postsQueryAdapter;
+    public double sum = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,6 +59,51 @@ public class TransactionActivity extends Activity{
                 receiveLoan();
             }
         });
+
+        ParseQueryAdapter.QueryFactory<Transaction> transactionQueryFactory =
+                new ParseQueryAdapter.QueryFactory<Transaction>(){
+                    public ParseQuery<Transaction> create(){
+                        ParseQuery<Transaction> query = Transaction.getQuery();
+                        query.include("user");
+                        query.whereEqualTo("friend", friendID);
+                        query.orderByDescending("created_at");
+                        return query;
+                    }
+                };
+
+        postsQueryAdapter = new ParseQueryAdapter<Transaction>(this, transactionQueryFactory){
+            @Override
+            public View getItemView(Transaction post, View view, ViewGroup parent){
+                if (view == null) {
+                    if (post.getType() == false){
+                        view = View.inflate(getContext(), R.layout.message_left, null);
+                        sum = sum + post.getAmount();
+                    }
+                    else{
+                        view = View.inflate(getContext(), R.layout.message_right, null);
+                        sum -= post.getAmount();
+                    }
+                }
+                //TextView usernameView = (TextView) view.findViewById(R.id.username_view);
+                //usernameView.setText(post.getFirstName() + " " + post.getLastName());
+
+                TextView dateView = (TextView) view.findViewById(R.id.date);
+                TextView amountView = (TextView) view.findViewById(R.id.amount);
+
+                SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+                dateView.setText(df.format(post.getCreatedAt()));
+                sum += post.getAmount();
+                amountView.setText(post.getAmount()+"");
+
+                return view;
+            }
+        };
+
+        ListView transactionList = (ListView) findViewById(R.id.listTransactions);
+        transactionList.setAdapter(postsQueryAdapter);
+
+        TextView sumText = (TextView) findViewById(R.id.sum);
+        sumText.setText("$ " + sum);
     }
 
     private void issueLoan(){
